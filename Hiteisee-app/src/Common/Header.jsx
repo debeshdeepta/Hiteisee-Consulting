8bba1226a6a534de4776ed3acf0a40d4ee13166b
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-07-30 22:06:58 +0530

    added Career In Nav bar

--- Hiteisee-app/src/Common/Header.jsx
+++ Hiteisee-app/src/Common/Header.jsx
@@ -20,6 +20,9 @@ export const Header = () => {
                 <a href="#about">About</a>
               </li>
               <li>
+                <a href="#career">Career</a>
+              </li>
+              <li>
                 <a href="#services">Services</a>
               </li>
               <li>
