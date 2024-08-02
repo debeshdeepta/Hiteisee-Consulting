5cfe2a89c7aca65a7fecd1871be0d1336950e290
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-08-02 17:53:22 +0530

    Contact us

--- Hiteisee-app/src/App.jsx
+++ Hiteisee-app/src/App.jsx
@@ -8,19 +8,21 @@ import Customers from './Componets/Customers';
 import AboutUs from './Componets/AboutUs';
 import Careers from './Componets/Careers';
 
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ContactUs from './Componets/ContactUs';
 
 function App() {
   return (
     <Router>
       <Header />
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/customers" element={<Customers />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/careers" element={<Careers />} />
+        <Route path="/Contact" element={<ContactUs />} />
       </Routes>
 
       <Footer />
--- Hiteisee-app/src/Common/Header.jsx
+++ Hiteisee-app/src/Common/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../Common/Header.css";
 import { Link } from "react-router-dom";
 
+
 export const Header = () => {
       
     return (
@@ -11,14 +12,17 @@ export const Header = () => {
           <img src="/HiteiseeLogo.png" alt="Logo" />
         </div>
 
+       
+
         <header className="header">
           <nav className="navbar">
             <ul className="navbar-links">
               <li > 
              <Link to="/">Home</Link>
               </li>
-              <li >
-                <Link to="/about">About Us</Link> 
+             
+              <li>
+              <Link to="/services">Services</Link>
               </li>
               <li>
               <Link to="/careers">Career</Link>
@@ -28,6 +32,9 @@ export const Header = () => {
               </li>
               <li>
               <Link to="/contact">Contact</Link>
+              </li>
+              <li >
+                <Link to="/about">About Us</Link> 
               </li>
             </ul>
           </nav>
