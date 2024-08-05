9fd53da65646e53aca4103c3e58f3642b60e5c6f
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-08-05 11:10:51 +0530

    header email and consulting button added

--- Hiteisee-app/src/Common/Header.jsx
+++ Hiteisee-app/src/Common/Header.jsx
@@ -2,38 +2,57 @@ import React from "react";
 import "../Common/Header.css";
 import { Link } from "react-router-dom";
 
-
 export const Header = () => {
-      
-    return (
-      
+  return (
     <div>
       <div className="top-logo">
         <div className="header-logo">
+          <div className="logo">
           <img src="/HiteiseeLogo.png" alt="Logo" />
+          </div>
+
+          <div className="contact-container">
+            <div className="contact-item">
+              <div className="contact-icon email-icon"></div>
+              <div className="contact-text">
+                <div className="contact-title">Email</div>
+                <div className="contact-detail">youremail@email.com</div>
+              </div>
+            </div>
+
+            <div className="contact-item">
+              <div className="contact-icon phone-icon"></div>
+              <div className="contact-text">
+                <div className="contact-title">Call</div>
+                <div className="contact-detail">+1235 2355 98</div>
+              </div>
+            </div>
+           
+            <button className="consulting-button">Free Consulting</button>
+          </div>
         </div>
 
         <header className="header">
           <nav className="navbar">
             <ul className="navbar-links">
-              <li > 
-             <Link to="/">Home</Link>
-              </li>
-             
               <li>
-              <Link to="/services">Services</Link>
+                <Link to="/">Home</Link>
               </li>
+
               <li>
-              <Link to="/careers">Career</Link>
+                <Link to="/services">Services</Link>
               </li>
               <li>
-              <Link to="/blog">Blog</Link>
+                <Link to="/careers">Career</Link>
               </li>
               <li>
-              <Link to="/contact">Contact</Link>
+                <Link to="/blog">Blog</Link>
               </li>
-              <li >
-                <Link to="/about">About Us</Link> 
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
               </li>
             </ul>
           </nav>
--- Hiteisee-app/src/Componets/Home.jsx
+++ Hiteisee-app/src/Componets/Home.jsx
@@ -1,28 +1,7 @@
-import React from 'react'
-import '../Componets/Home.css';
-const Home = () => {
-    return (
-        <div className='container'>
-            <div className="home">
-
-                <div className="heading-home">
-                    <p className='welcome'>Welcome to <span className='hiteisee'>Hiteisee</span></p>
-                    <h1 className='text'>We help to grow your Buiseness</h1>
-                </div>
-                <div className="button">
-                    Our Services
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Home
 // import React from 'react'
-// import './Home.css'
-
+// import '../Componets/Home.css';
 // const Home = () => {
-//   return (
+//     return (
 //         <div className='container'>
 //             <div className="home">
 
@@ -35,8 +14,29 @@ export default Home
 //                 </div>
 //             </div>
 //         </div>
-//   )
+//     )
 // }
 
 // export default Home
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+        <div className='container'>
+            <div className="home">
+
+                <div className="heading-home">
+                    <p className='welcome'>Welcome to <span className='hiteisee'>Hiteisee</span></p>
+                    <h1 className='text'>We help to grow your Buiseness</h1>
+                </div>
+                <div className="button">
+                    Our Services
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Home
 
