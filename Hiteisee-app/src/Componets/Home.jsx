86d98b1c002792700547607285864d3dd6a1ebf2
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-08-05 11:25:20 +0530

    conflicts fixed

--- Hiteisee-app/src/Componets/Home.jsx
+++ Hiteisee-app/src/Componets/Home.jsx
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react'
-// import '../Componets/Home.css';
-// const Home = () => {
-//     return (
-//         <div className='container'>
-//             <div className="home">
-
-//                 <div className="heading-home">
-//                     <p className='welcome'>Welcome to <span className='hiteisee'>Hiteisee</span></p>
-//                     <h1 className='text'>We help to grow your Buiseness</h1>
-//                 </div>
-//                 <div className="button">
-//                     Our Services
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Home
-import React from 'react'
-import './Home.css'
-
-const Home = () => {
-  return (
-        <div className='container'>
-            <div className="home">
-
-                <div className="heading-home">
-                    <p className='welcome'>Welcome to <span className='hiteisee'>Hiteisee</span></p>
-                    <h1 className='text'>We help to grow your Buiseness</h1>
-                </div>
-                <div className="button">
-                    Our Services
-                </div>
-            </div>
-        </div>
-  )
-}
-
-export default Home
-
-=======
 import React from 'react';
 import './Home.css'; 
 import { Link } from 'react-router-dom';
@@ -63,4 +19,3 @@ const Home = () => {
 }
 
 export default Home;
->>>>>>> 689d482e29b0aab1ac7f50384119f7cc158cc5c2
